7b717eeeab1a092145a3c499fc336a5ae3b2bc54
veloura-hair <munachiprosperity142@gmail.com>
2026-03-07 01:19:07 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,60 @@
+let cart=[]
+let total=0
+
+function addToCart(name,price){
+
+cart.push({name,price})
+
+total+=price
+
+updateCart()
+
+}
+
+function updateCart(){
+
+let list=document.getElementById("cart-items")
+
+list.innerHTML=""
+
+cart.forEach(item=>{
+
+let li=document.createElement("li")
+
+li.innerText=item.name+" - ₦"+item.price
+
+list.appendChild(li)
+
+})
+
+document.getElementById("total").innerText="Total: ₦"+total
+
+}
+
+function openCart(){
+
+document.getElementById("cart").style.right="0"
+
+}
+
+function closeCart(){
+
+document.getElementById("cart").style.right="-100%"
+
+}
+
+function checkout(){
+
+let message="Hello I want to order:%0A"
+
+cart.forEach(item=>{
+
+message+=item.name+" - ₦"+item.price+"%0A"
+
+})
+
+message+="Total: ₦"+total
+
+window.location.href="https://wa.me/2348166511475?text="+message
+
+}
